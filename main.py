922021823eebd6d08b360e263c16fd6df39f7a53
bossygit <bienvenu.kitutu@gmail.com>
2021-10-14 20:32:51 +0100

    Get score

--- main.py
+++ main.py
@@ -1,18 +1,57 @@
 from random import shuffle
-
+# Set is a list within {}, unordered unindexed, do not allow duplicate values.
 def createDeck():
     Deck = []
-
-    for i in range(0,4):
-        faceValues = ["A","J","Q","K"]
+    faceValues = ["A", "J", "Q", "K"]
+    for i in range(4):
         for card in range(2,11):
             Deck.append(card)
 
         for card in faceValues:
             Deck.append(card)
-        return Deck
-cardDeck = createDeck()
-print(cardDeck)
-shuffle(cardDeck)
-print("Shuffled")
-print(cardDeck)
+    return Deck
+
+class Player():
+    def __init__(self,hand= [],money= 100):
+        self.hand = hand
+        self.score = 0
+        self.money = money
+
+    def __str__(self):
+        currentHand =""
+
+        for card in self.hand:
+            currentHand += card + " "
+        finalStatus = currentHand + "score : " + str(self.score)
+
+        return finalStatus
+
+    def setScore(self):
+
+        faceValueDict = {"A":11,
+                         "J":10,
+                         "K":10,
+                         "Q":10,
+                         "2":2,
+                         "3":3,
+                         "4":4,
+                         "5":5,
+                         "6":6,
+                         "7":7,
+                         "8":8,
+                         "9":9,
+                         "10":10}
+        aceCounter = 0
+
+        for card in self.hand:
+            self.score += faceValueDict[card]
+
+
+
+
+
+
+
+
+
+
